2383659082179028523e42714cc3034e39ea364c
Stepanenko Sergey <powersa@mail.ru>
2025-07-11 11:06:12 +0300

    init commit

---
+++ hello/src/main/java/com/resetsa/helloworld/Helper.java
@@ -0,0 +1,12 @@
+package com.resetsa.helloworld;
+
+public class Helper {
+    private String Message;
+    public String getMessage() {
+        return this.Message;
+    }
+
+    public Helper(String message) {
+        this.Message = message;
+    }
+}
